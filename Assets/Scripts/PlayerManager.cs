557e7c7953c16f9eca073409d6f93df58e4cbbe1
PowerExpert <b.a.kanat2010@gmail.com>
2026-04-21 20:12:59 +0500

    yay

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,35 +1,96 @@
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 using System.Collections.Generic;
+
+[System.Serializable]
+public struct TextComponents
+{
+    public GameObject firstText;
+    public GameObject secondText;
+}
 
 public class PlayerManager : MonoBehaviour
 {
+    public GameObject canvasJoin;
+
     public GameObject tutorText;
     public GameObject timerText;
-
+    public List<TextComponents> texts = new List<TextComponents>();
     public timer timerScript;
-
-    public List<GameObject> players = new List<GameObject>();
+    public List<GameObject> playerPrefabs = new List<GameObject>();
     public PlayerInputManager manager;
 
-    public int playerCount;
+    private int playerCount = 0;
 
     void Start()
     {
         timerText.SetActive(false);
         tutorText.SetActive(true);
+
+        if (playerPrefabs.Count > 0)
+        {
+            manager.playerPrefab = playerPrefabs[0];
+        }
     }
-    void Update()
+
+    void OnEnable()
     {
         manager.onPlayerJoined += OnPlayerJoined;
     }
+
+    void OnDisable()
+    {
+        manager.onPlayerJoined -= OnPlayerJoined;
+    }
+
     private void OnPlayerJoined(PlayerInput playerInput)
     {
-        manager.playerPrefab = players[playerCount];
         tutorText.SetActive(false);
         timerText.SetActive(true);
-        timerScript.tick = true;
+
+        if (timerScript != null) timerScript.tick = true;
+
+        if (playerCount < texts.Count)
+        {
+            GameObject firstObj = texts[playerCount].firstText;
+            if (firstObj != null)
+            {
+                var textComponent = firstObj.GetComponent<Graphic>();
+                if (textComponent != null)
+                {
+                    if(playerCount % 2 != 0)
+                        textComponent.color = Color.blue;
+                    else
+                        textComponent.color = Color.red;
+                }
+            }
+
+            GameObject secondObj = texts[playerCount].secondText;
+            if (secondObj != null)
+            {
+                secondObj.SetActive(true);
+            }
+        }
+
         playerCount++;
+
+        if (playerCount < playerPrefabs.Count)
+        {
+            manager.playerPrefab = playerPrefabs[playerCount];
+        }
+    }
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            OnFinishPressed();
+            manager.DisableJoining();
+        }
+    }
+    public void OnFinishPressed()
+    {
+        canvasJoin.SetActive(false);
+        manager.DisableJoining();
     }
 }
